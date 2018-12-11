aed41551ae201732d524dbfe8c3223d995c0de7b
Aditya Naik <naik.aditya@gmail.com>
2018-12-11 10:35:39 +0100

    Updates tailwind css file with classes, adds content and css classes to header,footer index and hello js pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './css/tailwind.css';
 import React from "react"
 import ReactDOM from "react-dom"
 import Hello from "./Hello"
@@ -6,12 +7,16 @@ import Footer from "./Footer"
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Hello />
-            <Footer />
+        <div className="page-wrapper">
+            <div className="page-content">
+                <Header />
+                <div className="content-wrapper">
+                    <Hello />
+                </div>
+                <Footer />
+            </div >
         </div >
-    )
+)
 };
 
 ReactDOM.render(<App />, document.getElementById("app"))
